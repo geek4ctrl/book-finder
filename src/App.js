0e4227452501a7f2ebfcd3a7eb4366c99534f247
Laurent Mbuyu <laurent.mb19@gmail.com>
2019-02-28 01:03:20 +0200

    Displaying cards

--- src/App.js
+++ src/App.js
@@ -9,23 +9,28 @@ class App extends Component {
     super(props);
 
     this.state = {
-      items: []
+      items: [],
+      isLoaded: false
     }
   }
 
   componentDidMount(){
     fetch("https://www.googleapis.com/books/v1/volumes?q=robert kiyosaki&key=AIzaSyAcSMLOh0LMkos8jk-64mDRfcNOCLuj8fk")
-      .then(res => res.json())
+      .then(res => {
+          if (res.ok)
+          {
+            return res.json();
+          } else {
+            throw Error(res.statusText);
+          }
+      })
       .then(
-        (result) => {
-              for (let i = 0; i < 10; i++)
-              {
-                this.state.items.push({
-                  title: result.items[i].volumeInfo.title,
-                  authors: result.items[i].volumeInfo.authors,
-                  publisher: result.items[i].volumeInfo.publisher
-                })
-              }
+        (json) => {
+            this.setState({
+              items: json.items,
+              isLoaded: true
+            })
+            console.log(json.items);
         },
         (error) => {
               //console.log(error);
@@ -34,36 +39,33 @@ class App extends Component {
   }
 
   render() {
-
-    //console.log("Testing");
-    //console.log(this.state.items[0].volumeInfo);
-
+    //console.log(this.state.items);
+       const { isLoaded, items } = this.state;
+      //console.log(items);
     return (
       <div className="App">
             <div  className="animation">
                     <h1>BOOK FINDER</h1>
             </div>
-            
-            <input className="search-book" type="search" placeholder="Type your book here"/>
-            <input className="search-button" type="button" value="SEARCH"/>
+              <input className="search-book" type="search" placeholder="Type your book here"/>
+              <input className="search-button" type="button" value="SEARCH"/>
 
-           {/* Creating the book element*/}
+            <div>
+              {items.map(item => {
+                   return (
+                   <div className="card">
+                        <div className="book-image"><img src=""/> </div>
 
-          <div> {/* This will be used to separate cards in twos */}
+                        <div className="book-description"> 
+                          <div className="book-title">{ item.volumeInfo.title } </div>
+                          <div className="book-title">{ item.volumeInfo.title } </div>
+                          <div className="book-author">{ item.volumeInfo.authors } </div>
+                          <div className="book-publisher">{ item.volumeInfo.publisher } </div>
+                        </div>
+                   </div>)
+              } )}
+            </div>
 
-              <div className="card">
-                  <div className="book-image">Image</div>
-                  <div className="book-description">
-                      <div className="book-title">Harry Potter and the Cursed Child - Parts One and Two (Special Rehearsal Edition) </div>
-                      <div className="book-author">By: No authors found</div>
-                      <div className="book-publisher">Published By: Pottermore Publishing</div>
-
-                      <div className="see-more-button"><button className="see-more">See this book</button></div>
-                  </div>
-              </div>
-
-
-          </div>
       </div>
     );
   }
